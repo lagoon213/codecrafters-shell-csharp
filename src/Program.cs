4116ce71e6de491b31a40acaa7b2eaab910d6364
lagoon213 <standowicz.roksana@gmail.com>
2026-08-25 18:51:11 +0200

    codecrafters submit [skip ci]

--- src/Program.cs
+++ src/Program.cs
@@ -4,5 +4,8 @@ class Program
     {
         // TODO: Uncomment the code below to pass the first stage
         Console.Write("$ ");
+
+        string command = Console.ReadLine();
+        Console.WriteLine($"{command}: command not found");
     }
 }
